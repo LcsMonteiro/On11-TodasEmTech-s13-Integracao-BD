b2c49bce271ff2a00fd4cec8fb63d8cffc61ca8c
Larissa <lcsmonteiiro@gmail.com>
2021-06-04 10:19:46 -0300

    Atualizando

---
+++ crud-mongo/server.js
@@ -0,0 +1,13 @@
+const express = require('express') // importa express
+const app = express() //cria a instancia do express
+
+//conectar o mongo com o mongoose
+const db = require("./src/data/database")
+db.connect()
+
+app.use(express.json())
+
+//usar as rotas
+const livrosRouter = require("./src/routes/livros.routes")
+
+app.listen(3333, ()=> console.log('Servidor rodando'))    //subindo o servidor
---
+++ crud-mongo/src/routes/livros.routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controllers/livrosController')
+
+//Create/Criar -> Post
+router.post('/', controller.criaLivro)
+
+//Read/Ler -> Get
+router.get('/', controller.listaLivros)
+
+//Update/atualizar -> Patch
+router.patch('/:id', controller.atualizaLivro)
+
+//Delete/deletar -> Delete
+router.delete('/:id', controller.deletaLivro)
+
+module.exports = router
